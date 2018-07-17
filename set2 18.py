17476f5c230c6f266cd8b4941f7b2c29bd03c16e
vinaybadari <40800889+vinaybadari@users.noreply.github.com>
2018-07-17 23:04:53 +0530

    Update set2 18.py

--- set2 18.py
+++ set2 18.py
@@ -2,11 +2,11 @@ x,y=input().split()
 x=int(x)
 y=int(y)
 for a in range(x,y):
-	temp=a
-	num=0
+	t=a
+	n=0
 	while a!=0:
 		b=a%10
 		a=a//10
-		num += b**3
-	if num==temp:
-		print(temp,end=" ")
+		n += b**3
+	if n==t:
+		print(t,end=" ")
