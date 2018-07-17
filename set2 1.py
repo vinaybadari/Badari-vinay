4d49fce8d1a015f7896376d198af89d69b222390
vinaybadari <40800889+vinaybadari@users.noreply.github.com>
2018-07-17 09:36:39 +0530

    Update set2 1.py

--- set2 1.py
+++ set2 1.py
@@ -3,6 +3,6 @@ def power(b,e):
         return(b)
     if(e!=1):
         return(b*power(b,e-1))
-b=int(input("Enter base: "))
-e=int(input("Enter exponential value: "))
-print("Result:",power(b,e))
+b=int(input(" "))
+e=int(input(""))
+print(power(b,e))
