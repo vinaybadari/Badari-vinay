0dcb22f9fbba5922e40260da27f3f10d8ca1a874
vinaybadari <40800889+vinaybadari@users.noreply.github.com>
2018-07-17 09:49:32 +0530

    Update set2 1.py

--- set2 1.py
+++ set2 1.py
@@ -1,8 +1,4 @@
-def power(b,e):
-    if(e==1):
-        return(b)
-    if(e!=1):
-        return(b*power(b,e-1))
-b=int(input(" "))
-e=int(input(""))
-print(power(b,e))
+b=int(input())
+e=int(input())
+p=b**e
+print(p)
