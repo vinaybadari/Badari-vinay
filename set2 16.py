6d5d4da3e786e097a87e4102fe9508bad6066fb4
vinaybadari <40800889+vinaybadari@users.noreply.github.com>
2018-07-17 22:45:54 +0530

    Update set2 16.py

--- set2 16.py
+++ set2 16.py
@@ -1,11 +1,10 @@
-lower = int(input("Enter lower range: "))
-upper = int(input("Enter upper range: "))
- 
-for x in range(lower,upper + 1):
-   # prime numbers are greater than 1
-   if x > 1:
-       for i in range(2,num):
-           if (x % i) == 0:
-               break
-       else:
-           print(x)
+x,v=input().split()
+x=int(x)
+v=int(v)
+for z in range(x+1,v):
+	count=0
+	for i in range(1,z+1):
+		if z%i==0:
+			count=count+1
+	if count==2:
+		print (z,end=" ")
